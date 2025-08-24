00d7d6d568bb2b8f985d310af8eea6796c50b545
Lokesh Dalvi <dalvilokesh14@gmail.com>
2025-08-24 15:22:58 +0530

    Cache

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -3,6 +3,7 @@ import JobsListings from "@/components/JobsListings";
 import type { Job } from "@/lib/types"; 
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
 async function JobsPage() {
   const jobs: Job[] = await prisma.job.findMany({
     orderBy: { createdAt: "desc" },
